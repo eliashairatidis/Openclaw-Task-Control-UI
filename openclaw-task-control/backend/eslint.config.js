43e99c018a2cf078031eb648a6df5ecdef8072c5
eliashairatidis <149687236+eliashairatidis@users.noreply.github.com>
2026-02-20 21:55:48 -0500

    Add package script targets and end-to-end validation runbook

---
+++ openclaw-task-control/backend/eslint.config.js
@@ -0,0 +1,21 @@
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const parser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+    },
+  },
+];
